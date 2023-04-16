7a25b70c0790b9ca1dc6d84441cb5a2e983d6ae3
andrewr50 <51382718+andrewr50@users.noreply.github.com>
2023-04-16 14:58:15 -0500

    first commit

---
+++ Develop/script.js
@@ -0,0 +1,22 @@
+// Assignment code here
+
+
+generateBtn.addEventListener('click', function() {
+
+});
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+var modalSave = document.querySelector("#modalSave");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
